7cb4678c817fc3309444dea53133f4bb2f0c9d1c
Requiem <Requiem@DESKTOP-1ULQVKB>
2024-03-27 12:45:58 +0200

    Add project files.

---
+++ MRSTWEb.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MRSTWEb
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").
+                Include("~/Content/style.css"));
+
+
+            bundles.Add(new Bundle("~/bundles/main/js").Include(
+                "~/Scripts/script.js"));
+
+        }
+    }
+}
---
+++ MRSTWEb.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MRSTWEb.MvcApplication" Language="C#" %>
