c5e7235d1320b9fa5ff6594d754190e693e06831
MelonEditor <petrugolban4@gmail.com>
2024-04-20 15:35:07 +0300

    add interfaces domain, identity and startup file

---
+++ MRSTWEb.Domain/Interfaces/IClientManager.cs
@@ -0,0 +1,17 @@
+﻿using MRSTWEb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSTWEb.Domain.Interfaces
+{
+    public interface IClientManager : IDisposable
+    {
+        void Create(ClientProfile item);
+        void Delete(ClientProfile item);
+        void UpdateClientProfile(ClientProfile item);
+        ClientProfile GetClientProfileById(string Id);
+    }
+}
---
+++ MRSTWEb.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using MRSTWEb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSTWEb.Domain.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Delete(int id);
+        void Update(T item);
+        IEnumerable<Order> GetAllOrdersWithUsers(string userId);
+        IEnumerable<Order> GetOrdersByUserId(string userId);
+    }
+}
