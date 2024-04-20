64747053dde054a7c5f4956b3b88f752990ca4a5
MelonEditor <petrugolban4@gmail.com>
2024-04-20 16:12:34 +0300

    added DTO and infrastructures in bl

---
+++ MRSTWEb.BusinessLogic/Infrastructure/OperationDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSTWEb.BuisnessLogic.Infrastructure
+{
+    public class OperationDetails
+    {
+        public bool Succeeded { get; private set; }
+        public string Message { get; private set; }
+        public string Property { get; private set; }
+        public OperationDetails(bool succeded, string message, string prop)
+        {
+            Succeeded = succeded;
+            Message = message;
+            Property = prop;
+        }
+    }
+}
