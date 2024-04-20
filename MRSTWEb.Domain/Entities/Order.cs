ed83f2bf3aac855428ffb848c179bb483ef47ba1
Josan Oleg <s5rfcoleg@gmail.com>
2024-04-20 15:05:39 +0300

    Database added

---
+++ MRSTWEb.Domain/Entities/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRSTWEb.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public decimal TotalSumToPay { get; set; }
+        public DateTime BuyingTime { get; set; }
+
+        //Relatia cu utilizatorul
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        //Relatia cu produsul cumparat
+        public ICollection<OrderItem> Items { get; set; }
+
+        public Order()
+        {
+            Items = new List<OrderItem>();
+        }
+
+    }
+}
